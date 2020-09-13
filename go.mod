c0b1efd6f6be0ced39da4a964b68d771ce8f53cd
Bruno Ramalho <bramalho@divante.pl>
2020-09-13 15:47:33 +0200

    web app

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module tv-guide
 
 go 1.14
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.1
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.3.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+	"tv-guide/handlers"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(err)
+	}
+}
 
 func main() {
-	fmt.Println("It works!")
+	router := mux.NewRouter()
+	router.HandleFunc("/", handlers.HomeHandler).Methods("GET")
+
+	port, exists := os.LookupEnv("PORT")
+	if exists == false {
+		port = "8080"
+	}
+
+	srv := &http.Server{
+		Handler:      router,
+		Addr:         ":" + port,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	var err error
+
+	log.Println("Application is running on localhost:" + port)
+
+	log.Fatal(srv.ListenAndServe())
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
